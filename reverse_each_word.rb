8eea09769f0adfcc6394c4fc898be1cc152b2ec1
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-01 21:25:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
 
   string_array = string.split(" ")
+<<<<<<< HEAD
   reversed_array = []
   
   string_array.collect { |i| reversed_array << i.reverse!}
@@ -11,3 +12,22 @@ def reverse_each_word(string)
   
 end
 
+=======
+  
+  reversed_array = []
+  
+  string_array.each { |i| string_array[i].reverse}
+
+  #reversed_array
+  
+  
+end
+
+
+# Scrap Code
+
+original_string = "Hello world."
+
+puts reverse_each_word(original_string)
+
+>>>>>>> 6e684a26cb51de41eb85d830515986d49bb93afe
